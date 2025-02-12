789dbdcf2460d545b215f2f4d0fd60eaab26578f
Vivekmoyaje <vivekmoyaje@gmail.com>
2025-02-12 11:32:36 +0530

    file added

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,6 @@ export default defineConfig({
     outDir: "build", // This makes sure Vite outputs to the 'build' folder
   },
 
-  base: "/machine-coding-tasks", // Add this line
+  base: "/debounced_search-app", // Add this line
   plugins: [tailwindcss()],
 });
